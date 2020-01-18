c9caee59d5d2409333dc86d7c9ad98af8d514abb
manideepkamarajugadda <manideepkamarajugadda@gmail.com>
2020-01-18 16:49:42 -0600

    Initial commit

---
+++ src/main/java/com/joker/testweb/demo/TestWebApplication.java
@@ -0,0 +1,15 @@
+package com.joker.testweb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestWebApplication.class, args);
+        System.out.println("\n\n\n\n-----------THE TEST APPLICATION---------------\n\n\n");
+    }
+
+}
